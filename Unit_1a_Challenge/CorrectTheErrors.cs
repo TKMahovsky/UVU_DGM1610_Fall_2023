8b8c529ec2ef901118745fa27d983ce3f922c904
TKMahovsky <129914417+TKMahovsky@users.noreply.github.com>
2023-09-08 16:22:01 -0600

    Create CorrectTheErrors.cs

---
+++ Unit_1a_Challenge/CorrectTheErrors.cs
@@ -0,0 +1,13 @@
+//Use this file to store your work to demonstrate your understanding of operators
+//Make sure your work is different from the instructors examples or your peers
+using System;
+
+public class CorrectTheErrors
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("HW");
+    }
+}
+
+//System was misspelled, HW was missing a ", and there was } missing

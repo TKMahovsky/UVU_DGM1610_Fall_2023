6eddd37e292d61682d6d1d4a3478e70040d8f595
TKMahovsky <129914417+TKMahovsky@users.noreply.github.com>
2023-09-19 20:55:34 -0600

    Create Pyramid.cs

---
+++ Unit_1c_Lab/Pyramid.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Program
+{
+	
+    public void Main()
+    {
+		Console.WriteLine("Please type a number to build your pyramid:");
+		int userNum = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("\nHere is your pyramid!\n");
+		
+		for (int i = 1; i <= userNum; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(i);
+            }
+		Console.WriteLine();
+		}
+    }
+}

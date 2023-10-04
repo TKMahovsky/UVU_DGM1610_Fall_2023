0ed8f56d7fa8ea1d7f3a85c11c19c4916dcc3922
TKMahovsky <tkmahovsky@gmail.com>
2023-10-04 00:32:44 -0600

    Tweaking features

--- Unit4a_UFO/Assets/Scripts/PlayerController.cs
+++ Unit4a_UFO/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,27 +8,39 @@ public class PlayerController : MonoBehaviour
 
     public float horizontalInput;
     public float speed = 35;
-    public float xRange = 20;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public float xRange = 21;
+    public Transform blaster;
+    public GameObject laserBolt;
+    
     // Update is called once per frame
     void Update()
     {
+        //Move left and right
         horizontalInput = Input.GetAxis("Horizontal");
-        
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-
+        
+        //Left player bounds
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
+        
+        //Right player bounds
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        
+        //Create laser bolt with space bar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(laserBolt, blaster.transform.position, laserBolt.transform.rotation);
+        }
+    }
+
+    //Delete trigger objects that hit player
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
     }
 }
